7a7ea27b68f911fc2f4cc543d62e2bb13ec72b0f
ericswraps <ericmackey2506@gmail.com>
2024-04-07 18:10:04 -0400

    Calculator design

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    (b === 0) ? "Undefined" : a / b;
+}
+
+let number1;
+let number2;
+let operator;
+
+function operate(a, b, operator) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
+    }
+}
