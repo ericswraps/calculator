3bd44d550b42547d2e4731b00ed5583755eae04c
ericswraps <ericmackey2506@gmail.com>
2024-04-08 09:46:08 -0400

    Calculator functionality

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    (b === 0) ? "Undefined" : a / b;
+    if (b === 0) {
+        return "Undefined";
+    } else {
+        return (a / b);
+    }
 }
 
 let number1;
@@ -23,9 +27,63 @@ function operate(a, b, operator) {
         return add(a, b);
     } else if (operator === "-") {
         return subtract(a, b);
-    } else if (operator === "*") {
+    } else if (operator === "X") {
         return multiply(a, b);
     } else if (operator === "/") {
         return divide(a, b);
     }
 }
+
+const btns = document.querySelectorAll(".btn");
+const display = document.querySelector(".display");
+
+btns.forEach(button => {
+    button.addEventListener('click', function () {
+        display.textContent += button.textContent;
+    })
+})
+
+const zero = document.querySelector('#zero');
+zero.addEventListener('click', function () {
+    display.textContent += zero.textContent;
+})
+
+const clear = document.querySelector("#clear");
+
+
+clear.addEventListener('click', function () {
+    display.textContent = "";
+})
+
+const equals = document.querySelector("#equals");
+
+equals.addEventListener('click', function () {
+    let text = display.textContent;
+    console.log(text);
+    let array = text.split('');
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === "+" ||
+            array[i] === "-" ||
+            array[i] === "X" ||
+            array[i] === "/"
+        ) {
+            operator = array[i];
+            let firstArray = array.slice(0, i);
+            console.log("First array before operator: " + firstArray);
+            let numberOneString = firstArray.join('');
+            console.log("Joined first values: " + numberOneString);
+            number1 = parseInt(numberOneString);
+            let secondArray = array.slice(i + 1, array.length - 1);
+            console.log("Second array: " + secondArray);
+            let numberTwoString = secondArray.join('');
+            number2 = parseInt(numberTwoString);
+        }
+    }
+    display.textContent = operate(number1, number2, operator);
+    console.log(array);
+
+})
+
+
+
+
